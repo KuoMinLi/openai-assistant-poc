e8966856f8c36a69451c4e95e861ffbe6c11faf0
KuoMin <tw.kuomin@gmail.com>
2024-08-28 14:21:01 +0800

    [FIX] Adjust body background color and color code settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        gray_6: "#141414",
+        gray_5: "#626262",
+        gray_4: "#9c9c9c",
+        gray_3: "#e3e3e3",
+        gray_2: "#f1f1f1",
+        gray_1: "#fafafa",
+        gray_0: "#fcfcfc",
+        white: "#ffffff",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
